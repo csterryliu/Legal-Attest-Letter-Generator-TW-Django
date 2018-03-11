f9cc8d6015dcc1ec9e1190f909520877b90d85d4
Terry_Liu <tkuterry.liu@gmail.com>
2018-03-11 20:39:22 +0800

    Add first runnable code

--- lal_web/lal_web/generator/views.py
+++ lal_web/lal_web/generator/views.py
@@ -1,6 +1,8 @@
+import logging
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-import logging
+
+from lal_web.generator.lal_module import core
 
 logger = logging.getLogger('lal_web')
 
@@ -10,18 +12,30 @@ def main_page(request):
 
 
 def generate(request):
-    sender = request.POST.get('sender')
-    sender_addr = request.POST.get('senderAddr')
-    receiver = request.POST.get('receiver')
-    receiver_addr = request.POST.get('receiverAddr')
-    cc = request.POST.get('cc')
-    cc_addr = request.POST.get('ccAddr')
+    senders = [[request.POST.get('sender')]]
+    senders_addr = [request.POST.get('senderAddr')]
+    receivers = [[request.POST.get('receiver')]]
+    receivers_addr = [request.POST.get('receiverAddr')]
+    ccs = [[request.POST.get('cc')]]
+    cc_addr = [request.POST.get('ccAddr')]
     content = request.POST.get('content')
-    logger.debug(sender)
-    logger.debug(sender_addr)
-    logger.debug(receiver)
-    logger.debug(receiver_addr)
-    logger.debug(cc)
+    logger.debug(senders)
+    logger.debug(senders_addr)
+    logger.debug(receivers)
+    logger.debug(receivers_addr)
+    logger.debug(ccs)
     logger.debug(cc_addr)
     logger.debug(content)
+    text_path, letter_path = core.generate_text_and_letter(senders,
+                                                           senders_addr,
+                                                           receivers,
+                                                           receivers_addr,
+                                                           ccs,
+                                                           cc_addr,
+                                                           content)
+    logger.debug(text_path)
+    logger.debug(letter_path)
+    core.merge_text_and_letter(text_path, letter_path, 'test.pdf')
+    core.clean_temp_files(text_path, letter_path)
+    logger.debug('done')
     return HttpResponseRedirect('/')
