b00b509157eab69f6e1027ee512106c1e1bf9d0b
Terry_Liu <tkuterry.liu@gmail.com>
2018-04-08 13:49:32 +0800

    Dismiss modal on confirm

--- lal_web/static/js/controls.js
+++ lal_web/static/js/controls.js
@@ -1,11 +1,17 @@
 function save_info() {
     console.log('save info');
     var sender = $('#input_sender').val();
+    $('#input_sender').val('');
     var sender_addr = $('#input_senderAddr').val();
+    $('#input_senderAddr').val('');
     var receiver = $('#input_receiver').val();
+    $('#input_receiver').val('');
     var receiver_addr = $('#input_receiverAddr').val();
+    $('#input_receiverAddr').val('');
     var cc = $('#input_cc').val();
+    $('#input_cc').val('');
     var cc_addr = $('#input_ccAddr').val();
+    $('#input_ccAddr').val('');
     var num_of_cards = $('div .row .card.border-primary .btn.btn-danger').length;
     console.log(num_of_cards);
     $.get(`/add_info/?sender=${sender}&senderAddr=${sender_addr}&receiver=${receiver}&receiverAddr=${receiver_addr}&cc=${cc}&ccAddr=${cc_addr}&num_of_info=${num_of_cards}`, function(ret_value){
