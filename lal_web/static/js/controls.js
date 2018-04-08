88486bd595743a4531aba0780282fc39db3c6c31
Terry_Liu <tkuterry.liu@gmail.com>
2018-04-08 14:43:23 +0800

    Users are able to delete one info card

--- lal_web/static/js/controls.js
+++ lal_web/static/js/controls.js
@@ -1,5 +1,4 @@
 function save_info() {
-    console.log('save info');
     var sender = $('#input_sender').val();
     $('#input_sender').val('');
     var sender_addr = $('#input_senderAddr').val();
@@ -13,10 +12,12 @@ function save_info() {
     var cc_addr = $('#input_ccAddr').val();
     $('#input_ccAddr').val('');
     var num_of_cards = $('div .row .card.border-primary .btn.btn-danger').length;
-    console.log(num_of_cards);
     $.get(`/add_info/?sender=${sender}&senderAddr=${sender_addr}&receiver=${receiver}&receiverAddr=${receiver_addr}&cc=${cc}&ccAddr=${cc_addr}&num_of_info=${num_of_cards}`, function(ret_value){
         var card_html = ret_value;
+        console.log(card_html);
         $(card_html).insertBefore('#card_add_info');
+        var button = $('#card_add_info').prev().find('button');
+        button.click(delete_an_info);
     });
 }
 
@@ -27,5 +28,14 @@ function delete_all_info() {
         for (var idx = 0; idx < all_info_cards.length-1; idx++) {
             all_info_cards.eq(idx).remove();
         }
+    }
+}
+
+function delete_an_info() {
+    var card_number = parseInt($(this).prev().html().replace('#', '')) - 1;
+    $(this).parent().parent().parent().remove();
+    var all_info_cards = $('div .row .col-sm-4');
+    for (var idx=card_number; idx < all_info_cards.length-1; idx++) {
+        all_info_cards.eq(idx).find('span').html('#'+(idx+1));
     }
 }
