5ba46a4eee98964d27e86388231e311fdeb8d40e
Terry_Liu <tkuterry.liu@gmail.com>
2018-05-17 23:33:52 +0800

    modify settings

--- lal_web/lal_web/settings.py
+++ lal_web/lal_web/settings.py
@@ -120,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals(), databases=None, test_runner=False)
