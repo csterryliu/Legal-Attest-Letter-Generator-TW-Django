7431405b95f004fdfcdee9e748bd22f0d1e45f16
Terry_Liu <tkuterry.liu@gmail.com>
2018-05-17 23:55:48 +0800

    modify constants

--- lal_web/lal_web/generator/lal_module/constants.py
+++ lal_web/lal_web/generator/lal_module/constants.py
@@ -5,8 +5,8 @@ Constants which will be used in the main logic.
 import os
 
 BASE_HEROKU_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-LETTER_FORMAT_PATH = os.path.join(BASE_HEROKU_DIR, 'lal_web/lal_web/generator/lal_module/res/tw_lal.pdf')
-DEFAULT_FONT_PATH = os.path.join(BASE_HEROKU_DIR, 'lal_web/lal_web/generator/lal_module/res/TW-Kai-98_1.ttf')
+LETTER_FORMAT_PATH = os.path.join(BASE_HEROKU_DIR, 'lal_module/res/tw_lal.pdf')
+DEFAULT_FONT_PATH = os.path.join(BASE_HEROKU_DIR, 'lal_module/res/TW-Kai-98_1.ttf')
 _PDF_INCH = 72
 ########################################################################
 # pre-defined coordinates of main article
