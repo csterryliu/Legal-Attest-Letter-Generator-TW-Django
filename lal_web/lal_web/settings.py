3ed09a5a6b678be2bdb7fb1211cb36baee041001
Terry_Liu <tkuterry.liu@gmail.com>
2022-10-16 12:20:57 +0800

    Try to fix the path of static files

--- lal_web/lal_web/settings.py
+++ lal_web/lal_web/settings.py
@@ -127,6 +127,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
