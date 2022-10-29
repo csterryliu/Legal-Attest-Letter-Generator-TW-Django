50e18571ff887e95c7836ea1f9cac51917d8af1e
Terry_Liu <tkuterry.liu@gmail.com>
2022-10-29 12:42:22 +0800

    Try to fix runtime error

--- lal_web/lal_web/settings.py
+++ lal_web/lal_web/settings.py
@@ -131,5 +131,6 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
