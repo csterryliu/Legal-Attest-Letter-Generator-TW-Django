704303eb25cf222e4c180dde38b728d9ab63a2dd
Terry_Liu <tkuterry.liu@gmail.com>
2022-10-29 13:00:50 +0800

    Try to fix build error

--- lal_web/lal_web/settings.py
+++ lal_web/lal_web/settings.py
@@ -127,8 +127,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+else:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, 'static'),
+    )
