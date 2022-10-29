91ee4ab66d5b12a7e26ab973add29291131731f0
Terry_Liu <tkuterry.liu@gmail.com>
2022-10-29 13:07:31 +0800

    Try to enhance the setting

--- lal_web/lal_web/settings.py
+++ lal_web/lal_web/settings.py
@@ -126,11 +126,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-else:
-    STATICFILES_DIRS = (
+STATICFILES_DIRS = (
         os.path.join(BASE_DIR, 'static'),
     )
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
